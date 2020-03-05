7e35d30ad4d582ae48897d9a234e2136966a832d
Marcel Hauser <maha93@gmx.de>
2020-03-05 17:48:10 +0100

    added base plugin files

---
+++ src/translations/en/test-plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * test-plugin plugin for Craft CMS 3.x
+ *
+ * craft cms test plugin
+ *
+ * @link      https://ice-creme.de
+ * @copyright Copyright (c) 2020 Marcel Hauser
+ */
+
+/**
+ * test-plugin en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('test-plugin', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Marcel Hauser
+ * @package   Testplugin
+ * @since     0.0.1
+ */
+return [
+    'test-plugin plugin loaded' => 'test-plugin plugin loaded',
+];
